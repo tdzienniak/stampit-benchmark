b0f22b3060a1cb0ba738dab631f3c3e29903521c
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2016-10-28 15:59:17 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,93 @@
+const Benchmark = require('benchmark');
+const stampit = require('stampit');
+
+const suite = new Benchmark.Suite;
+
+const COUNT = 5000;
+const DELTA = 16;
+
+
+const Entity = stampit({
+  init() {
+    this.components = {};
+  },
+});
+
+const Position = stampit({
+  init() {
+    this.x = 10;
+    this.y = 10;
+  },
+});
+
+
+const Velocity = stampit({
+  init() {
+    this.vx = 10;
+    this.vy = 10;
+  },
+});
+
+const stampitEntities = ((count) => {
+  const arr = [];
+
+  for (let i = 0; i < count; i++) {
+    const e = Entity();
+
+    e.components.position = Position();
+    e.components.velocity = Velocity();
+
+    arr.push(e);
+  }
+
+  return arr;
+})(COUNT);
+
+const normalEntities = ((count) => {
+  const arr = [];
+
+  for (let i = 0; i < count; i++) {
+    arr.push({
+      components: {
+        position: {
+          x: 10,
+          y: 10,
+        },
+        velocity: {
+          vy: 10,
+          vx: 10,
+        },
+      },
+    });
+  }
+
+  return arr;
+})(COUNT);
+
+console.log('Running benchmark...');
+
+suite.add('Stampit', () => {
+  for (let i = 0; i < COUNT; i++) {
+    const entity = stampitEntities[i].components;
+
+    entity.position.x += DELTA / 1000 * entity.velocity.vx;
+    entity.position.y += DELTA / 1000 * entity.velocity.vy;
+  }
+})
+.add('Plain object', () => {
+  for (let i = 0; i < COUNT; i++) {
+    const entity = normalEntities[i].components;
+
+    entity.position.x += DELTA / 1000 * entity.velocity.vx;
+    entity.position.y += DELTA / 1000 * entity.velocity.vy;
+  }
+})
+// add listeners
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async
+.run({ 'async': true });
